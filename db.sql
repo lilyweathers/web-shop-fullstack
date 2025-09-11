c40962b15354bfe98e77e420c9bdffb08f4c05f0
Lily Laptop <lily.weathers87@gmail.com>
2025-09-11 14:47:26 +0200

    v1.6.1

--- db.sql
+++ db.sql
@@ -10,16 +10,17 @@ CREATE TABLE product (
 	description VARCHAR(255),
 	name VARCHAR(255),
 	price DOUBLE NOT NULL,
+	img VARCHAR(255),
 	PRIMARY KEY (id)
 );
 
-INSERT INTO product (category, description, name, price) VALUES ('Linen', 'White linen fabric', 'White linen', 300.0);
-INSERT INTO product (category, description, name, price) VALUES ('Knit', 'Soft grey knitted fabric', 'Grey knit', 250.0);
-INSERT INTO product (category, description, name, price) VALUES ('Wool', 'Warm beige wool fabric', 'Beige wool', 200.0);
-INSERT INTO product (category, description, name, price) VALUES ('Silk', 'Smooth black silk fabric', 'Black silk', 400.0);
-INSERT INTO product (category, description, name, price) VALUES ('Fur', 'Leopard fur fabric', 'Animal fur', 300.0);
-INSERT INTO product (category, description, name, price) VALUES ('Silk', 'Soft gold silk fabric', 'Gold silk', 250.0);
-INSERT INTO product (category, description, name, price) VALUES ('Cloth', 'Terry cloth fabric', 'Terry cloth', 200.0);
-INSERT INTO product (category, description, name, price) VALUES ('Cloth', 'Vivid pink cloth fabric', 'Pink cloth', 400.0);
-INSERT INTO product (category, description, name, price) VALUES ('Cloth', 'Dark blue cloth fabric', 'Blue cloth', 250.0);
+INSERT INTO product (category, description, name, price, img) VALUES ('Linen', 'White linen fabric', 'White linen', 300.0, '/images/linen_texture.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Knit', 'Soft grey knitted fabric', 'Grey knit', 250.0, '/images/knit_texture.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Wool', 'Warm beige wool fabric', 'Beige wool', 200.0, '/images/wool_texture.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Silk', 'Smooth black silk fabric', 'Black silk', 400.0, '/images/black_fabric.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Fur', 'Leopard fur fabric', 'Animal fur', 300.0, '/images/animal_print.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Silk', 'Soft gold silk fabric', 'Gold silk', 250.0, '/images/silky_brown_fabric.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Cloth', 'Terry cloth fabric', 'Terry cloth', 200.0, '/images/gray_texture.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Cloth', 'Vivid pink cloth fabric', 'Pink cloth', 400.0, '/images/pink_fabric.jpg');
+INSERT INTO product (category, description, name, price, img) VALUES ('Cloth', 'Dark blue cloth fabric', 'Blue cloth', 250.0, '/images/dark_blue_fabric.jpg');
 
